c342cbabe189e1cb22ffb909c1d7dbd2ef3d782f
samirafisaleh <sami.rafi.saleh@gmail.com>
2022-06-05 18:40:22 -0700

    Initial Commit

---
+++ PeriodicTable-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PeriodicTable-iOS
+//
+//  Created by Sami Saleh on 6/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
